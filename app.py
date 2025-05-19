6fdb601e6ac827bc00ae46ab63eae2c9a8d2042a
Aayush <gittets@gmail.com>
2025-05-19 09:15:18 +0000

    template path update and dockerfile added

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 
-Calc1 = Flask(__name__)
+Calc1 = Flask(__name__, template_folder='Templates')
 
 @Calc1.route('/')
 def index():
@@ -26,4 +26,4 @@ def Calculating():
         
     return render_template('CalcFront.html', result=result)
 if __name__ == '__main__':
-    Calc1.run(debug=True) 
+    Calc1.run(debug=True, host='0.0.0.0', port=5000) 
