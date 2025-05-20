cd5154f1cf3338fefac0c431e4dc4b3f35a34891
Aayush <gittets@gmail.com>
2025-05-20 08:23:41 +0000

    replace Calc1 to app

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, request
 from prometheus_flask_exporter import PrometheusMetrics
-Calc1 = Flask(__name__, template_folder='Templates')
-metrics = PrometheusMetrics(Calc1)
+app = Flask(__name__, template_folder='Templates')
+metrics = PrometheusMetrics(app)
 
-@Calc1.route('/')
+@app.route('/')
 def index():
     return render_template('CalcFront.html')
 
-@Calc1.route('/Calculating', methods=['POST'])
+@app.route('/Calculating', methods=['POST'])
 def Calculating():
     number1 = float(request.form['num1'])
     number2 =float(request.form['num2'])
@@ -27,4 +27,4 @@ def Calculating():
 
     return render_template('CalcFront.html', result=result)
 if __name__ == '__main__':
-    Calc1.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
